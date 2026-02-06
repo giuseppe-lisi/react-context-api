b557c2c40682b5f7fc6d3979034945fe515ff198
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-02-06 18:28:39 +0100

    aggiunto bottone modalità budget

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { BudgetContext } from "../contexts/BudgetContext";
 
 function Navbar() {
+    const { budgetMode, setBudgetMode } = useContext(BudgetContext);
+
     return (
         <div className="navbarDiv">
             <div className="wrapper navbar">
-                <h1>CoseCheNonServono Store</h1>
+                <div>
+                    <h1>CoseCheNonServono Store</h1>
+                </div>
                 <nav>
                     <NavLink to="/">Home</NavLink>
                     <NavLink to="/Products">Prodotti</NavLink>
                     <NavLink to="/AboutUs">Info</NavLink>
+                    <button
+                        className="budgetButton"
+                        onClick={() => setBudgetMode(!budgetMode)}
+                    >
+                        Modalità Budget: {budgetMode ? "On" : "Off"}
+                    </button>
                 </nav>
             </div>
         </div>
