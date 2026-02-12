31d5faca1cb3da77f5fbe911d82e2a7c9e803728
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-02-12 13:10:37 +0100

    viene ora mostrato un numero di stelle corrispondente al rating

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,29 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
 function ProductCard({ product }) {
+    const rating = Math.ceil(product.rating.rate);
+    const [stars, setStars] = useState([]);
+
+    useEffect(()=>{
+        const tempStarsArray = [];
+        for (let i = 1; i <= rating; i++) {
+            tempStarsArray.push(i);
+        }
+        setStars(tempStarsArray);
+    }, [])
+
 
     return (
         <>
-            <Link
-                to={`/Products/${product.id}`}
-                className="productCard"
-                key={product.id}
-            >
+            <Link to={`/Products/${product.id}`} className="productCard">
                 <div className="cardImg">
                     <img src={product.image} />
                 </div>
                 <h4>{product.title}</h4>
+                {stars.map((star) => (
+                    <img className="star" src="src/assets/images/star.png" />
+                ))}
             </Link>
         </>
     );
